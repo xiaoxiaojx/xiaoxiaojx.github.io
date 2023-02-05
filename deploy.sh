3c9c898745b23737afbd9ff703da9e32926924a5
xiaoxiaojx <784487301@qq.com>
2023-02-05 22:02:55 +0800

    u

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,6 @@ folder="../xiaoxiaojx.github.io"
 if [[ -d $folder ]]; then
   cd $folder;
   cp -r /Users/duoxiaokai/work/me/my-blog/out/* /Users/duoxiaokai/work/xiaoxiaojx.github.io/;
-  mv /Users/duoxiaokai/work/xiaoxiaojx.github.io/_next /Users/duoxiaokai/work/xiaoxiaojx.github.io/next
-  sed -i '' -e 's/_next/next/g' `grep -rl /_next/ /Users/duoxiaokai/work/xiaoxiaojx.github.io/`
   echo 发布完成;
 else
   echo '目录不存在';
