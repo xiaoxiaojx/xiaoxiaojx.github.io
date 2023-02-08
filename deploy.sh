abfefe76807e5849b60e0e9f571ddc4b896f1ba2
xiaoxiaojx <784487301@qq.com>
2023-02-09 00:39:25 +0800

    u

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,8 @@ folder="../xiaoxiaojx.github.io"
 
 if [[ -d $folder ]]; then
   cd $folder;
+  rm -rf /Users/duoxiaokai/work/xiaoxiaojx.github.io/posts
+  rm -rf /Users/duoxiaokai/work/xiaoxiaojx.github.io/_next
   cp -r /Users/duoxiaokai/work/me/my-blog/out/* /Users/duoxiaokai/work/xiaoxiaojx.github.io/;
   echo 发布完成;
 else
